ed5f9104a8e76dc50de563f18a9dbe889443a803
kamioka24 <keee2404@gmail.com>
2021-06-19 22:11:42 +0900

    [First Commit]

---
+++ app/src/main/java/com/example/recyclerview/Memo.kt
@@ -0,0 +1,6 @@
+package com.example.recyclerview
+
+data class Memo (
+    val content: String,
+    val date: String
+)
---
+++ app/src/main/java/com/example/recyclerview/MemoViewAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.recyclerview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoViewAdapter(private val context: Context, private val memoList: List<Memo>) : RecyclerView.Adapter<MemoViewAdapter.MemoViewHolder>() {
+
+    class MemoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val userIcon = view.findViewById<ImageView>(R.id.userIcon)
+        val userName = view.findViewById<TextView>(R.id.userName)
+        val date = view.findViewById<TextView>(R.id.date)
+        val content = view.findViewById<TextView>(R.id.content)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemoViewHolder =
+        MemoViewHolder(LayoutInflater.from(context).inflate(R.layout.memo, parent, false))
+
+    override fun getItemCount(): Int = memoList.size
+
+    override fun onBindViewHolder(holder: MemoViewHolder, position: Int) {
+        holder.userIcon.setImageResource(R.mipmap.ic_launcher)
+        holder.userName.text = "ヒカキン"
+        holder.date.text = memoList[position].date
+        holder.content.text = memoList[position].content
+    }
+}
