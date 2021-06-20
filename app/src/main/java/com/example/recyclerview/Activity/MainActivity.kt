71bbd50ead960e965aa2efd20764458d5c9f6469
kamioka24 <keee2404@gmail.com>
2021-06-20 22:24:01 +0900

    [Finish]

--- app/src/main/java/com/example/recyclerview/Activity/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/Activity/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.recyclerview
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
-import android.widget.Toolbar
-import androidx.core.view.drawToBitmap
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,9 +18,15 @@ class MainActivity : AppCompatActivity() {
         val goToTopButton = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.goToTopButton)
         val goToBottomButton = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.goToBottomButton)
         val memoRecyclerView = findViewById<RecyclerView>(R.id.memoRecyclerView)
+
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
         toolbar.setLogo(R.mipmap.ic_launcher) // ツールバーにロゴを設定
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar) // アクションバーにツールバーを設定
+
+        val toolbarLayout = findViewById<com.google.android.material.appbar.CollapsingToolbarLayout>(R.id.toolbarLayout)
+        toolbarLayout.title = getString(R.string.app_name)
+        toolbarLayout.setExpandedTitleColor(Color.WHITE)
+        toolbarLayout.setCollapsedTitleTextColor(R.color.teal_200) // 縮小時の文字色
 
         // データの生成
         val date = SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Date())
@@ -37,10 +42,12 @@ class MainActivity : AppCompatActivity() {
             recyclerView.layoutManager = LinearLayoutManager(this)
         }
 
+        // 一番上へスクロール
         goToTopButton.setOnClickListener {
             memoRecyclerView.smoothScrollBy(0, -100000)
         }
 
+        // 一番下へスクロール
         goToBottomButton.setOnClickListener {
             memoRecyclerView.smoothScrollBy(0, 100000)
         }
